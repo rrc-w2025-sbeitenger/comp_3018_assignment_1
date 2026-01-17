6cf51bac62f738af137370ff9fcf342ea899fb17
Sven Beitenger <sbeitenger@academic.rrc.ca>
2026-01-17 15:45:09 -0600

    feat: Within app.ts added healthCheckResponse interface, and created a health check endpoint function that confirms the server is running.

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express, {Express} from "express";
+
+//Initialize Express application, configuring the app before it starts.
+//Express prevents endpoints from not sending a response object back. Since all endpoints must reply back.
+const app: Express = express();
+
+/*
+* Represents the response structure for the health check endpoint.
+*/
+interface healthCheckResponse {
+    status: String;
+    uptime: Number;
+    timestamp: String;
+    version: String;
+}
+
+//Define a route.
+//Request object receives information from the client, response object is what the api sends back.
+app.get("/src/api/v1/routes", (req, res) => {
+    //response object that matches our healthCheckResponse interface.
+    const healthData: healthCheckResponse = {
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0"
+    }
+
+    res.json(healthData);
+});
+
+//export app to use within server.ts.
+export default app;
